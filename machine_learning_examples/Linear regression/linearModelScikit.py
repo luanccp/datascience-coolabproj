4e10f90a8f37d9f9dca4b0947f94d4e3bc27f0dd
Luan Castheloge <luanccp@gmail.com>
2018-12-26 16:26:23 -0200

    [ADD] New scripts

--- machine_learning_examples/Linear regression/linearModelScikit.py
+++ machine_learning_examples/Linear regression/linearModelScikit.py
@@ -3,6 +3,7 @@
 Created on Thu Nov  8 11:27:10 2018
 
 @author: Luan
+@description: application for linear regression with random data
 """
 
 from sklearn.linear_model import LinearRegression
@@ -15,30 +16,26 @@ Y = np.linspace(0,12,7).reshape(-1,1)
 lr = LinearRegression().fit(X,Y)
 
 print ("Coeficiente: {}".format(lr.coef_))
-# Intercepto é onde passa pela linha 0
+# Intercepto is when across zero point
 print ("Intercepto: {}".format(lr.intercept_))
 
 plt.plot(X, Y)
 plt.grid(True)
-#plt.show()   
 
-
-# Criando dados para regressão
 from sklearn.datasets import make_regression
 X,Y = make_regression(n_samples=200, n_features=1, noise=10, random_state=5)
 
 plt.scatter(X, Y)
-#plt.show()
-
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, Y, random_state=5)
 lr = LinearRegression().fit(X_train, y_train)
 
-# Plotando o grafico
-linha = np.linspace(-3, 3, 500).reshape(-1, 1) # gerar a linha da predição
+# creating the chart
+linha = np.linspace(-3, 3, 500).reshape(-1, 1) 
 plt.plot(X_train, y_train, '^', markersize=5)
 plt.plot(X_test, y_test, 'v', markersize=7)  
 plt.plot(linha, lr.predict(linha))
 plt.grid(True)
 plt.title("Modelo linear\n Treino: {:.2f} - Teste: {:.2f}".format(lr.score(X_train, y_train), lr.score(X_test, y_test)))
+plt.show()
